1a1df3140589c0766c2d6b3cc00655ea336b1df4
Christian Lebrio <ChristianLebrio@gmail.com>
2022-12-30 00:46:04 -0500

    Added CSS and finished functions. Various minor bug fixes

--- index.js
+++ index.js
@@ -6,49 +6,49 @@ function johnRegular(){
 }
 
 function roseGodTier(){
-    document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ae/Godtierrose.png/revision/latest?cb=20120319084152"
  }
 function roseRegular(){
     document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/7/7e/Rose_Lalonde.png/revision/latest?cb=20100710062733"
 }
 
 function daveGodTier(){
-    document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/d/dd/GodTierDave.png/revision/latest?cb=20120728151447"
  }
 function daveRegular(){
     document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/e/e8/Dave_Strider.png/revision/latest?cb=20101027044211"
 }
 
 function jadeGodTier(){
-    document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/3/35/Godtierjadesprite.png/revision/latest?cb=20130622180246"
  }
 function jadeRegular(){
     document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ad/Jade_Harley.gif/revision/latest?cb=20100826013411"
 }
 
 function janeGodTier(){
-    document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/b/b0/God_Tier_Jane_sprite.png/revision/latest?cb=20141218032336"
  }
 function janeRegular(){
     document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/f/f4/Jane_Crocker.png/revision/latest?cb=20130109194446"
 }
 
 function roxyGodTier(){
-    document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/0/01/Roxy_godtier.png/revision/latest?cb=20130824221152"
  }
 function roxyRegular(){
     document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/c/c3/Roxy_Lalonde.png/revision/latest?cb=20111117064128"
 }
 
 function dirkGodTier(){
-    document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/8/83/God_tier_Dirk.png/revision/latest?cb=20150725030948"
  }
 function dirkRegular(){
     document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/5/58/Dirk_Strider.png/revision/latest?cb=20171109025839"
 }
 
 function jakeGodTier(){
-    document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/9/98/Jake%27s_symbolic_god_tier.png/revision/latest?cb=20200726032013"
  }
 function jakeRegular(){
     document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ae/Jake_English.png/revision/latest?cb=20111111200652"
