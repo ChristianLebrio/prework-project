e9b7c1b6a4844183773e620066fb919efa9e3294
Christian Lebrio <ChristianLebrio@gmail.com>
2022-12-29 22:00:48 -0500

    Added readme. Redefined functions in HTML. Started to write functions in linked script

---
+++ index.js
@@ -0,0 +1,55 @@
+function johnGodTier(){
+    document.getElementById("johnImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function johnRegular(){
+    document.getElementById("johnImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/e/ec/John_Egbert2.png/revision/latest?cb=20160517194150"
+}
+
+function roseGodTier(){
+    document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function roseRegular(){
+    document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/7/7e/Rose_Lalonde.png/revision/latest?cb=20100710062733"
+}
+
+function daveGodTier(){
+    document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function daveRegular(){
+    document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/e/e8/Dave_Strider.png/revision/latest?cb=20101027044211"
+}
+
+function jadeGodTier(){
+    document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function jadeRegular(){
+    document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ad/Jade_Harley.gif/revision/latest?cb=20100826013411"
+}
+
+function janeGodTier(){
+    document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function janeRegular(){
+    document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/f/f4/Jane_Crocker.png/revision/latest?cb=20130109194446"
+}
+
+function roxyGodTier(){
+    document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function roxyRegular(){
+    document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/c/c3/Roxy_Lalonde.png/revision/latest?cb=20111117064128"
+}
+
+function dirkGodTier(){
+    document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function dirkRegular(){
+    document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/5/58/Dirk_Strider.png/revision/latest?cb=20171109025839"
+}
+
+function jakeGodTier(){
+    document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+ }
+function jakeRegular(){
+    document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ae/Jake_English.png/revision/latest?cb=20111111200652"
+}
