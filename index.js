7507b3908e280994f99b8a63bea3294f515764cd
Christian Lebrio <ChristianLebrio@gmail.com>
2022-12-30 01:06:32 -0500

    Hosted images locally

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 function johnGodTier(){
-    document.getElementById("johnImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/2/2a/John_Godtier_-_Hood_down.png/revision/latest?cb=20101230092211"
+    document.getElementById("johnImg").src = "imgs/JohnGodtier.png"
  }
 function johnRegular(){
-    document.getElementById("johnImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/e/ec/John_Egbert2.png/revision/latest?cb=20160517194150"
+    document.getElementById("johnImg").src = "imgs/John.png"
 }
 
 function roseGodTier(){
-    document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ae/Godtierrose.png/revision/latest?cb=20120319084152"
+    document.getElementById("roseImg").src = "imgs/RoseGodtier.png"
  }
 function roseRegular(){
-    document.getElementById("roseImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/7/7e/Rose_Lalonde.png/revision/latest?cb=20100710062733"
+    document.getElementById("roseImg").src = "imgs/Rose.png"
 }
 
 function daveGodTier(){
-    document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/d/dd/GodTierDave.png/revision/latest?cb=20120728151447"
+    document.getElementById("daveImg").src = "imgs/DaveGodtier.png"
  }
 function daveRegular(){
-    document.getElementById("daveImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/e/e8/Dave_Strider.png/revision/latest?cb=20101027044211"
+    document.getElementById("daveImg").src = "imgs/Dave.png"
 }
 
 function jadeGodTier(){
-    document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/3/35/Godtierjadesprite.png/revision/latest?cb=20130622180246"
+    document.getElementById("jadeImg").src = "imgs/JadeGodtier.png"
  }
 function jadeRegular(){
-    document.getElementById("jadeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ad/Jade_Harley.gif/revision/latest?cb=20100826013411"
+    document.getElementById("jadeImg").src = "imgs/Jade.png"
 }
 
 function janeGodTier(){
-    document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/b/b0/God_Tier_Jane_sprite.png/revision/latest?cb=20141218032336"
+    document.getElementById("janeImg").src = "imgs/JaneGodtier.png"
  }
 function janeRegular(){
-    document.getElementById("janeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/f/f4/Jane_Crocker.png/revision/latest?cb=20130109194446"
+    document.getElementById("janeImg").src = "imgs/Jane.png"
 }
 
 function roxyGodTier(){
-    document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/0/01/Roxy_godtier.png/revision/latest?cb=20130824221152"
+    document.getElementById("roxyImg").src = "imgs/RoxyGodtier.png"
  }
 function roxyRegular(){
-    document.getElementById("roxyImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/c/c3/Roxy_Lalonde.png/revision/latest?cb=20111117064128"
+    document.getElementById("roxyImg").src = "imgs/Roxy.png"
 }
 
 function dirkGodTier(){
-    document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/8/83/God_tier_Dirk.png/revision/latest?cb=20150725030948"
+    document.getElementById("dirkImg").src = "imgs/DirkGodtier.png"
  }
 function dirkRegular(){
-    document.getElementById("dirkImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/5/58/Dirk_Strider.png/revision/latest?cb=20171109025839"
+    document.getElementById("dirkImg").src = "imgs/Dirk.png"
 }
 
 function jakeGodTier(){
-    document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/9/98/Jake%27s_symbolic_god_tier.png/revision/latest?cb=20200726032013"
+    document.getElementById("jakeImg").src = "imgs/JakeGodtier.png"
  }
 function jakeRegular(){
-    document.getElementById("jakeImg").src = "https://static.wikia.nocookie.net/mspaintadventures/images/a/ae/Jake_English.png/revision/latest?cb=20111111200652"
+    document.getElementById("jakeImg").src = "imgs/Jake.png"
 }
