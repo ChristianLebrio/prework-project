c6f195b8ddffb931ede1a3a1fcd19af0d19aee82
Christian Lebrio <ChristianLebrio@gmail.com>
2022-12-30 01:17:20 -0500

    Changed html and javacript to use internal images

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 function johnGodTier(){
-    document.getElementById("johnImg").src = "imgs/JohnGodtier.png"
+    document.getElementById("johnImg").src = "./imgs/JohnGodtier.png"
  }
 function johnRegular(){
-    document.getElementById("johnImg").src = "imgs/John.png"
+    document.getElementById("johnImg").src = "./imgs/John.png"
 }
 
 function roseGodTier(){
-    document.getElementById("roseImg").src = "imgs/RoseGodtier.png"
+    document.getElementById("roseImg").src = "./imgs/RoseGodtier.png"
  }
 function roseRegular(){
-    document.getElementById("roseImg").src = "imgs/Rose.png"
+    document.getElementById("roseImg").src = "./imgs/Rose.png"
 }
 
 function daveGodTier(){
-    document.getElementById("daveImg").src = "imgs/DaveGodtier.png"
+    document.getElementById("daveImg").src = "./imgs/DaveGodtier.png"
  }
 function daveRegular(){
-    document.getElementById("daveImg").src = "imgs/Dave.png"
+    document.getElementById("daveImg").src = "./imgs/Dave.png"
 }
 
 function jadeGodTier(){
-    document.getElementById("jadeImg").src = "imgs/JadeGodtier.png"
+    document.getElementById("jadeImg").src = "./imgs/JadeGodtier.png"
  }
 function jadeRegular(){
-    document.getElementById("jadeImg").src = "imgs/Jade.png"
+    document.getElementById("jadeImg").src = "./imgs/Jade.png"
 }
 
 function janeGodTier(){
-    document.getElementById("janeImg").src = "imgs/JaneGodtier.png"
+    document.getElementById("janeImg").src = "./imgs/JaneGodtier.png"
  }
 function janeRegular(){
-    document.getElementById("janeImg").src = "imgs/Jane.png"
+    document.getElementById("janeImg").src = "./imgs/Jane.png"
 }
 
 function roxyGodTier(){
-    document.getElementById("roxyImg").src = "imgs/RoxyGodtier.png"
+    document.getElementById("roxyImg").src = "./imgs/RoxyGodtier.png"
  }
 function roxyRegular(){
-    document.getElementById("roxyImg").src = "imgs/Roxy.png"
+    document.getElementById("roxyImg").src = "./imgs/Roxy.png"
 }
 
 function dirkGodTier(){
-    document.getElementById("dirkImg").src = "imgs/DirkGodtier.png"
+    document.getElementById("dirkImg").src = "./imgs/DirkGodtier.png"
  }
 function dirkRegular(){
-    document.getElementById("dirkImg").src = "imgs/Dirk.png"
+    document.getElementById("dirkImg").src = "./imgs/Dirk.png"
 }
 
 function jakeGodTier(){
-    document.getElementById("jakeImg").src = "imgs/JakeGodtier.png"
+    document.getElementById("jakeImg").src = "./imgs/JakeGodtier.png"
  }
 function jakeRegular(){
-    document.getElementById("jakeImg").src = "imgs/Jake.png"
+    document.getElementById("jakeImg").src = "./imgs/Jake.png"
 }
